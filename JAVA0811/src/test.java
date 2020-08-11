2caf30f90c1e9bedb314cb539074187fe9753ded
ReadStar <ReadStar@users.noreply.github.com>
2020-08-11 11:47:23 +0900

    .

--- JAVA0811/src/test.java
+++ JAVA0811/src/test.java
@@ -8,6 +8,26 @@ public class test {
 			// =>Hello, world" 문자열을 10번 반복 출력하는 hello()메서드
 			hello();
 			gugudan();
+			System.out.println("============");
+			
+			//2. 매개변수는 없고, 리턴 값만 있는 메서드
+			// 1) 1 ~10까지 정수의 합(55)를 리턴하는 total()메서드
+			//		=>결과값인 정수 합이 리턴되므로 int형 변수 total에 저장
+			System.out.println("1 ~10 까지 정수 합" + total());
+			
+			// 2) "Hello, World" 문자열을 리턴하는 hello2()메서드
+			String str = hello2();
+			System.out.println("리턴받은 문자열 : " + str);
+			
+			System.out.println("-------------------------------");
+			//3. 매개변수만 있고, 리턴값은 없는 메서드
+			// 1) 출력할 문자열을 전달하면, 10번 반복 출력하는 print()메서드
+			print("Vindicate");
+			
+		 // 	2)정수  (n)을 전달하면, 1 ~ n까지의 합계를 출력하는 sum() 메서드
+//					ex) n = 10 55
+//							n = 100 5050
+			sum(100);
 	}
 	//1. 매개변수도 없고, 리턴값도 없는 메서드
 	// =>Hello, world" 문자열을 10번 반복 출력하는 hello()메서드의 정의
@@ -27,4 +47,36 @@ public class test {
 				}
 		}
 	}
+	//2. 매개변수는 없고 리턴값만 있는 메서드
+	public static int total() {
+		int sum = 0;
+		for (int i = 1; i <=10; i++) {
+			sum+=i;
+		}
+		return sum;
+	}
+	
+	
+	//2-2 매개변수는 없고 리턴값만 있는 메서드 호출
+	public static String hello2() {
+		return "Hello, World";
+	}
+	
+	//3. 매개변수만 있고, 리턴값은 없는 메서드
+	public static void print(String print) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i+ print);
+		}
+	}
+	
+	public static void sum(int n) {
+			int sum = 0;
+		for (int i = 1; i<=n; i++) {
+			sum+= i;
+		}
+		System.out.println(sum);
+	}
+	
+	
+	
 		}
